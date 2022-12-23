9c1b3fd60df05ddba1d45f635b8f371ef0aa25dd
ttakuma17 <ttanimiya@gmail.com>
2022-12-23 22:21:28 +0900

    ポケモンクラスの作成

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args){
+        Pokemon nyaoha = new Pokemon();
+        nyaoha.setName("ニャオハ");
+        nyaoha.setType1("くさ");
+        nyaoha.setHp(20);
+        nyaoha.setLevel(5);
+
+        System.out.println(nyaoha.getName() +"のレベルは"+ nyaoha.getLevel() + "です");
+        System.out.println(nyaoha.getName()+ "は「" + nyaoha.getType1() + "」タイプです");
+
+        nyaoha.setLevel(16);
+        System.out.println(nyaoha.getName() + "は 16 にレベルアップしました");
+        nyaoha.setName("ニャローテ");
+        System.out.println(nyaoha.getName() + "に進化しました" );
+        System.out.println(nyaoha.getName()+ "は「" + nyaoha.getType1() + "」タイプです");
+
+        nyaoha.setLevel(36);
+        System.out.println(nyaoha.getName() + "は 36 にレベルアップしました");
+        nyaoha.setName("マスカーニャ");
+        nyaoha.setType2("あく");
+        System.out.println(nyaoha.getName() + "に進化しました");
+        System.out.println(nyaoha.getName() + "は「" + nyaoha.getType1() + "/" + nyaoha.getType2() + "」タイプです");
+    }
+}
---
+++ src/Pokemon.java
@@ -0,0 +1,48 @@
+public class Pokemon {
+    private String name;
+    private String type1;
+    private String type2;
+    private int hp;
+    private int level;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setType1 (String type1){
+        this.type1 = type1;
+    }
+
+    public String getType1(){
+        return this.type1;
+    }
+
+    public void setType2 (String type2){
+        this.type2 = type2;
+    }
+
+    public String getType2(){
+        return this.type2;
+    }
+
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+
+    public int getHp(){
+        return this.hp;
+    }
+
+    public void setLevel(int hp){
+        this.hp = level;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+}
