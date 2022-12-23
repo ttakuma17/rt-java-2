fabd571ce8f7d147eb4d4e0512bd676599ff6efd
ttakuma17 <ttanimiya@gmail.com>
2022-12-23 22:42:56 +0900

    for文の追加

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,36 @@ public class Main {
         Pokemon nyaoha = new Pokemon();
         nyaoha.setName("ニャオハ");
         nyaoha.setType1("くさ");
-        nyaoha.setHp(20);
         nyaoha.setLevel(5);
 
         System.out.println(nyaoha.getName() +"のレベルは"+ nyaoha.getLevel() + "です");
         System.out.println(nyaoha.getName()+ "は「" + nyaoha.getType1() + "」タイプです");
 
+        // 5レベルから15まで繰り返し処理でレベルアップ
+        for(int i=0; i<10; i++){
+            nyaoha.setLevel(nyaoha.getLevel() + 1);
+            System.out.println(nyaoha.getName() +"のレベルは"+ nyaoha.getLevel() + "になりました");
+        }
+
         nyaoha.setLevel(16);
-        System.out.println(nyaoha.getName() + "は 16 にレベルアップしました");
+        System.out.println(nyaoha.getName() + "は 16 にレベルアップしました。おや..... ?");
+        System.out.println("・");
+        System.out.println("・");
+        System.out.println("・");
         nyaoha.setName("ニャローテ");
         System.out.println(nyaoha.getName() + "に進化しました" );
         System.out.println(nyaoha.getName()+ "は「" + nyaoha.getType1() + "」タイプです");
 
+        for (int i=0; i<19; i++){
+            nyaoha.setLevel(nyaoha.getLevel() + 1);
+            System.out.println(nyaoha.getName() +"のレベルは"+ nyaoha.getLevel() + "になりました");
+        }
+
         nyaoha.setLevel(36);
-        System.out.println(nyaoha.getName() + "は 36 にレベルアップしました");
+        System.out.println(nyaoha.getName() + "は 36 にレベルアップしました。おや..... ?");
+        System.out.println("・");
+        System.out.println("・");
+        System.out.println("・");
         nyaoha.setName("マスカーニャ");
         nyaoha.setType2("あく");
         System.out.println(nyaoha.getName() + "に進化しました");
